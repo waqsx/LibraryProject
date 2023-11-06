f6c3384685a2c0ef207a6757c8075c067f5f1c38
waqs <artemmerkuriev@yandex.ru>
2023-11-06 21:26:25 +0300

    Library

---
+++ LibraryProject/LibraryForms/AddAuthor.xaml.cs
@@ -0,0 +1,66 @@
+﻿using LibraryProject.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LibraryProject.LibraryForms
+{
+    /// <summary>
+    /// Логика взаимодействия для AddAuthor.xaml
+    /// </summary>
+    public partial class AddAuthor : Window
+    {
+        public AddAuthor()
+        {
+            InitializeComponent();
+
+
+
+
+
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            using (LibraryContext context = new())
+            {
+                string name = author_name.Text;
+                string lastname = author_lastname.Text;
+
+                if (author_lastname.Text != null && author_name != null)
+                {
+                    Author author = new Author(name, lastname);
+                    context.Authors.Add(author);
+                    context.SaveChanges();
+
+                    MessageBox.Show($"{name + ' ' + lastname} добавлен");
+                    author_name.Text = "";
+                    author_lastname.Text = "";
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Проверьте данные");
+                }
+            }
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+
+}
---
+++ LibraryProject/LibraryForms/AddGenre.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LibraryProject.LibraryForms
+{
+    /// <summary>
+    /// Логика взаимодействия для AddGenre.xaml
+    /// </summary>
+    public partial class AddGenre : Window
+    {
+        public AddGenre()
+        {
+            InitializeComponent();
+        }
+    }
+}
